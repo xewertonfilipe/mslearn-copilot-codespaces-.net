8fe5f6e950d5536bd0b184de1ea255a721e6ff6b
Ewerton Filipe <xewertonfilipe@gmail.com>
2026-06-18 12:50:58 -0300

    Add files via upload

---
+++ AccelerateDevGHCopilot/src/Library.Console/CommonActions.cs
@@ -0,0 +1,14 @@
+namespace Library.Console;
+
+[Flags]
+public enum CommonActions
+{
+    Repeat = 0,
+    Select = 1,
+    Quit = 2,
+    SearchPatrons = 4,
+    RenewPatronMembership = 8,
+    ReturnLoanedBook = 16,
+    ExtendLoanedBook = 32,
+    SearchBooks = 64
+}
